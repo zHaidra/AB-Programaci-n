29cdbbdb21c818b56374d536eb842b1281e3bf76
Miguel Cano <mcanodomingo@gmail.com>
2024-10-08 21:01:53 +0200

    Agregar archivos de proyecto.

---
+++ AB Programación.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int Add(int x, int y) {
+	cout << "En Add(), recibido " << x << " y " << y << "/n";
+	return (x + y);
+}
+
+
+int main() {
+	cout << "En main ()\n";
+	cout << "\nllamada a Add()\n";
+	cout << "El valor devuelto es: " << Add(3, 4);
+	cout << "\nvuelta a main (). \n";
+	cout << "\nsaliendo...\n";
+}
